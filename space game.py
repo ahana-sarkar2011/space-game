21ac9bfc2c3d221df459bb77f8fad054486b0422
ahana-sarkar2011 <ulyssesroad30@gmail.com>
2026-02-07 09:51:56 +0000

    Add files via upload

--- space game.py
+++ space game.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init()
 bg = pygame.image.load("lesson 5-space game/images/space.png")
+shootingsound = pygame.mixer.Sound("lesson 5-space game/images/Gun+Silencer.mp3")
+collision = pygame.mixer.Sound("lesson 5-space game/images/Grenade+1.mp3")
 WIDTH = 1000
 HEIGHT = 600
+redbullets = []
+yellowbullets = []
 redh = 10
 font = pygame.font.SysFont("rockwell",40)
 redhealth = font.render("HEALTH = "+str(redh),True,"red")
@@ -43,13 +47,51 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LCTRL:
+                shootingsound.play()
+                bullet = pygame.Rect(red.x+30,red.y+30,10,10)
+                redbullets.append(bullet)
+            if event.key == pygame.K_RCTRL:
+                shootingsound.play()
+                bullety = pygame.Rect(yellow.x+30,yellow.y+30,10,10)
+                yellowbullets.append(bullety)
+    
     keys_pressed = pygame.key.get_pressed()
     redmove()
     yellowmove()
     screen.blit(bg,(0,0))
     pygame.draw.rect(screen,"black",border)
     screen.blit(redss,red)
+    yellowhealth = font.render("HEALTH = "+str(yellowh),True,"yellow")
     screen.blit(yellowss,yellow)
+    redhealth = font.render("HEALTH = "+str(redh),True,"red")
     screen.blit(redhealth,(10,10))
     screen.blit(yellowhealth,(700,10))
+    for bullet in redbullets:
+        pygame.draw.rect(screen,"red",bullet)
+        bullet.x+=10
+        if bullet.colliderect(yellow):
+            redbullets.remove(bullet)
+            yellowh -= 1
+            collision.play()
+    for bullety in yellowbullets:
+        pygame.draw.rect(screen,"yellow",bullety)
+        bullety.x -= 10
+        if bullety.colliderect(red):
+            yellowbullets.remove(bullety)
+            redh -= 1
+            collision.play()
+    if redh<1:
+        ywins = font.render("YELLOW WINS",True,"yellow")
+        screen.blit(ywins,(300,260))
+        pygame.display.update()
+        pygame.time.delay(3000)
+        pygame.quit()
+    if yellowh<1:
+        rwins = font.render("RED WINS",True,"red")
+        screen.blit(rwins,(300,300))
+        pygame.display.update()
+        pygame.time.delay(3000)
+        pygame.quit()
     pygame.display.update()
